7bd57bf5807519db41362483afa7a2b6bb1571ad
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-05-14 14:25:24 +0300

    c

--- src/Days.jsx
+++ src/Days.jsx
@@ -8,9 +8,24 @@ class Days extends Component {
 
     render() {
         const dayEvents = [
-            { time: "02:00" },
+            { time: "00:00" },
             { time: "01:00" },
+            { time: "02:00" },
             { time: "03:00" },
+            { time: "04:00" },
+            { time: "05:00" },
+            { time: "06:00" },
+            { time: "07:00" },
+            { time: "08:00" },
+            { time: "09:00" },
+            { time: "10:00" },
+            { time: "11:00" },
+            { time: "12:00" },
+            { time: "13:00" },
+            { time: "14:00" },
+            { time: "15:00" },
+            { time: "16:00" },
+            { time: "17:00" },
         ]
         let i = 0;
 
@@ -28,7 +43,7 @@ class Days extends Component {
         // console.log(events);
         
 
-        for (let i = 0; i <= dayEvents.length; i++) {
+        for (let i = 0; i < dayEvents.length; i++) {
             Object.assign(dayEvents[i], events[i])
         }
 
